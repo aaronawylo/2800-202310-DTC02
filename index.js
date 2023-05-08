8478b06d11e278caa230df0562db5d30984b55ca
Aaron Lo <alo76@my.bcit.ca>
2023-05-08 14:47:33 -0700

    index.js skeleton commit

---
+++ index.js
@@ -0,0 +1,117 @@
+require('dotenv').config();
+require('./utils.js')
+const express = require('express');
+
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const bcrypt = require('bcrypt');
+
+// const usersModel = require('./models/users');
+
+const saltRounds = 12;
+
+const Joi = require('joi');
+
+let ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+// cookie expire time is one hour
+const expireTime = 1000 * 60 * 60;
+
+
+// Secret information section
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+// End of secret information section
+
+var {database} = include('databaseConnection')
+
+const usersModel = database.db(mongodb_database).collection('users')
+
+app.use(express.urlencoded({ extended: false }));
+
+var mongoStore = MongoStore.create({
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`,
+  crypto: {
+    secret: mongodb_session_secret
+  },
+});
+
+app.use(session({
+  secret: node_session_secret,
+  store: mongoStore,
+  saveUninitialized: false,
+  resave: false
+}));
+
+function isValidSession(req) {
+  if (req.session.authenticated) {
+      return true;
+  }
+  return false;
+}
+
+function sessionValidation(req,res,next) {
+  if (isValidSession(req)) {
+      next();
+  }
+  else {
+      res.redirect('/login');
+  }
+}
+
+
+function isAdmin(req) {
+  if (req.session.user_type == 'admin') {
+      return true;
+  }
+  return false;
+}
+
+function adminAuthorization(req, res, next) {
+  if (!isAdmin(req)) {
+      res.status(403);
+      res.render("errorMessage.ejs", {error: "Not Authorized For You"});
+      return;
+  }
+  else {
+      next();
+  }
+}
+
+app.get('/', (req, res) => {
+
+
+// Alex's code
+
+app.get()
+
+// End of Alex's code
+
+// Marco's code
+
+app.get()
+
+// End of Marco's code
+
+// Aaron's Code
+
+app.get()
+
+// End of Aaron's code
+
+// Derek's code
+
+app.get()
+
+// End of Derek's code
