35c2bb3e4d87105b3231339dbfa63433d5688e25
ALEX DESCHENES <adeschenes@my.bcit.ca>
2023-05-10 10:46:09 -0700

    Add signup functionality

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 // End of secret information section
 
-var {database} = include('databaseConnection')
+var { database } = include('databaseConnection')
 
 const usersModel = database.db(mongodb_database).collection('users')
 
@@ -54,41 +54,111 @@ app.use(session({
 
 function isValidSession(req) {
   if (req.session.authenticated) {
-      return true;
+    return true;
   }
   return false;
 }
 
-function sessionValidation(req,res,next) {
+function sessionValidation(req, res, next) {
   if (isValidSession(req)) {
-      next();
+    next();
   }
   else {
-      res.redirect('/login');
+    res.redirect('/login');
   }
 }
 
 
 function isAdmin(req) {
   if (req.session.user_type == 'admin') {
-      return true;
+    return true;
   }
   return false;
 }
 
 function adminAuthorization(req, res, next) {
   if (!isAdmin(req)) {
-      res.status(403);
-      res.render("errorMessage.ejs", {error: "Not Authorized For You"});
-      return;
+    res.status(403);
+    res.render("errorMessage.ejs", { error: "Not Authorized For You" });
+    return;
   }
   else {
-      next();
+    next();
   }
 }
 
 // Alex's code
+// Render the sign up form
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs');
+});
 
+// Handle sign up form submission
+app.post('/signup', async (req, res) => {
+  const {
+    email,
+    username,
+    password,
+    experience,
+  } = req.body;
+
+  // Validate input
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    experience: Joi.string().required(),
+  });
+  const validationResult = schema.validate({
+    email,
+    username,
+    password,
+    experience,
+  });
+  if (validationResult.error) {
+    res.status(400).send(`Invalid username or password characters. <a href="/">Go back to home</a>`);
+    return;
+  }
+
+  // Check if username already exists
+  const existingUser = await usersModel.findOne({
+    username: username
+  });
+  if (existingUser) {
+    res.status(409).send(`Username already exists. <a href="/">Go back to home</a>`);
+    return;
+  }
+  // Check if email already exists
+  const existingEmail = await usersModel.findOne({
+    email: email
+  });
+  if (existingEmail) {
+    res.status(409).send(`Email already exists. <a href="/">Go back to home</a>`);
+    return;
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  // Hash email
+  const hashedEmail = await bcrypt.hash(email, saltRounds);
+
+  // Create new user
+  const newUser = {
+    email: hashedEmail,
+    username: username,
+    password: hashedPassword,
+    experience: experience,
+    admin: false
+  };
+  await usersModel.insertOne(newUser);
+
+  // Log in user
+  req.session.user = newUser;
+
+  // Redirect to members area
+  res.redirect('/members');
+});
 
 
 // End of Alex's code
@@ -102,13 +172,13 @@ function adminAuthorization(req, res, next) {
 // Aaron's Code
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  if (isValidSession(req)){
+  if (isValidSession(req)) {
     res.render('index.ejs', {
       "loggedIn": true,
       "name": req.session.username,
     })
   }
-  else{
+  else {
     res.render('index.ejs', {
       "loggedIn": false
     })
@@ -120,12 +190,12 @@ app.get('/', (req, res) => {
 // Derek's code
 app.get('/login', (req, res) => {
   var invalidLogin = req.query.invalidLogin
-  if (isValidSession(req)){
-    res.render('login.ejs', {"loggedIn": true}, )
+  if (isValidSession(req)) {
+    res.render('login.ejs', { "loggedIn": true },)
   }
-  else{
-    res.render('login.ejs', {"loggedIn": false, "invalidLogin": invalidLogin}, )
-}
+  else {
+    res.render('login.ejs', { "loggedIn": false, "invalidLogin": invalidLogin },)
+  }
 })
 
 app.post('/loginSubmit', async (req, res) => {
@@ -134,20 +204,20 @@ app.post('/loginSubmit', async (req, res) => {
   const emailValidation = Joi.string().email().validate(req.body.email)
   const passwordValidation = Joi.string().max(20).validate(req.body.password)
   if (emailValidation.error != null || passwordValidation.error != null) {
-      res.redirect("/login?invalidLogin=true")
-      return
+    res.redirect("/login?invalidLogin=true")
+    return
   }
-  
+
   var user = await usersModel.find({ email: email })
   if (user.length > 0) {
-      const isMatch = bcrypt.compareSync(password, user[0].password)
-      if (!isMatch) { res.redirect(`/login?invalidLogin=true`) }
-      else {
-          req.session.authenticated = true
-          req.session.username = user[0].username
-          req.session.cookie.maxAge = 60 * 60 * 1000;
-          res.redirect('/members')
-      }
+    const isMatch = bcrypt.compareSync(password, user[0].password)
+    if (!isMatch) { res.redirect(`/login?invalidLogin=true`) }
+    else {
+      req.session.authenticated = true
+      req.session.username = user[0].username
+      req.session.cookie.maxAge = 60 * 60 * 1000;
+      res.redirect('/members')
+    }
   }
   else { res.redirect(`/login?invalidLogin=true`) }
 })
@@ -157,9 +227,9 @@ app.post('/loginSubmit', async (req, res) => {
 // End of Derek's code
 
 app.get("*", (req, res) => {
-    res.status(404).render("404.ejs");
-  });
-  
+  res.status(404).render("404.ejs");
+});
+
 app.listen(port, () => {
-    console.log("Listening on port " + port + "!");
-  });
+  console.log("Listening on port " + port + "!");
+});
